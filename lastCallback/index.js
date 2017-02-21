45ff49e5cfdb72ad77f0b79a18d982e16cf7ca0d
GGG <170418719@qq.com>
2017-02-21 22:26:19 +0800

    modify lastCallback.js

--- lastCallback/index.js
+++ lastCallback/index.js
@@ -10,6 +10,7 @@ obj.prototype = {
 		var callback_ = arguments[ arguments.length - 1 ];
 		callback = ( typeof( callback_ ) == 'function' ? callback_ : null );
 		
+		// 如果 arg2 不存在 ， 抛出错误		
 		if( !arg2 )	 callback( new Error( 'second argument missing or not a string' ) );
 
 		callback( null , arg1 );
