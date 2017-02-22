2332108b14e3f7e401487656c372d932e31bea07
gggxx <170418719@qq.com>
2017-02-22 14:37:00 +0800

    add middleware.js

--- http/middleware.js
+++ http/middleware.js
@@ -6,21 +6,59 @@
 
 // Connect中间件
 // 3.x版本中 connect.static 方法被分离
-var connect = require('connect'),
-       http = require('http'),
-    __dirname = 'home/example';
+// var connect = require('connect'),
+//        http = require('http'),
+//     __dirname = 'home/example';
 
-var app = connect() // 实在例化一个Connect
+// var app = connect() // 实在例化一个Connect
 
-app.use( connect.static( __dirname + 'public_html' ) , { redirect : true } )
-   .use( connect.logger() )
+// app.use( connect.static( __dirname + 'public_html' ) , { redirect : true } )
+//    .use( connect.logger() )
 
-http.createServer( app ).listen( 8124 );
+// http.createServer( app ).listen( 8124 );
 
 
 // Connect.favicon( path , options )的实现
 module.exports = function( path , options ){
 
-  
+	var options = options || {},
+	  path = path || __dirname + '/../public/favicon.ico',
+	  maxAge = options.maxAge || 86400000;
 
+	return function( req , res , next ){
+
+		if( '/favicon.ico' == req.url ){
+
+			if( icon ){
+				res.writeHead( 200 , icon.headers );
+				res.end( icon.body );
+			}else{
+
+				fs.readFile( path , function( err , buf ){
+
+					if( err )  return next( err );
+
+					icon = {
+						headers : {
+							'Content-type' : 'image/x-icon',
+							'Content-Length' : buf.length,
+							'ETag' : '"' + utils.md5( buf ) + '"',
+							'Cache-Control' : 'public , max-age=' + ( maxAge / 1000 )
+						},
+						body : buf
+					};
+
+					res.writeHead( 200 , icon.headers );
+					res.end( icon.body );
+
+				})
+
+			}
+		}else{
+			next();
+		}
+
+	}
 }
+
+
